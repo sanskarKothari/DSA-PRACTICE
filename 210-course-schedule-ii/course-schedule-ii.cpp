a5b18fc3eae76345ea976d2b9769fc500d3f149e
Sanskar Sachin Kothari <124ee0045@nitrkl.ac.in>
2026-07-12 12:15:32 +0530

    Time: 3 ms (71.05%) | Memory: 19 MB (27.18%) - LeetSync

---
+++ 210-course-schedule-ii/course-schedule-ii.cpp
@@ -0,0 +1,41 @@
+class Solution {
+public:
+    vector<int> findOrder(int n, vector<vector<int>>& prerequisites) {
+         vector<vector<int>> v(n);
+        
+        vector<int> ind(n,0);
+        for(auto it:prerequisites){
+            int u = it[0];
+            int k = it[1];
+            v[k].push_back(u);
+            ind[u]++;
+        }
+
+        queue<int> q;
+        for(int i=0;i<n;i++){
+            if(ind[i]==0){
+                q.push(i);
+            }
+        }
+
+        vector<int> ans;
+        while(!q.empty()){
+            int fr = q.front();
+            ans.push_back(fr);
+            q.pop();
+
+            for(auto it:v[fr]){
+                ind[it]--;
+                if(ind[it]==0){
+                    q.push(it);
+                }
+            }
+        }
+
+         if(ans.size()==n) return ans;
+         else{
+            return {};
+         }
+
+    }
+};
