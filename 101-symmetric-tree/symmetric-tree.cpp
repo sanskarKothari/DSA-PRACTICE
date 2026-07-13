f81d2b5286a7e39b75467fa4b5b22813238ea41a
Sanskar Sachin Kothari <124ee0045@nitrkl.ac.in>
2026-07-13 11:47:12 +0530

    Time: 0 ms (100.00%) | Memory: 18.6 MB (31.47%) - LeetSync

---
+++ 101-symmetric-tree/symmetric-tree.cpp
@@ -0,0 +1,21 @@
+class Solution {
+public:
+    bool isMirror(TreeNode* p, TreeNode* q) {
+        if (p == nullptr && q == nullptr)
+            return true;
+
+        if (p == nullptr || q == nullptr)
+            return false;
+
+        return (p->val == q->val) &&
+               isMirror(p->left, q->right) &&
+               isMirror(p->right, q->left);
+    }
+
+    bool isSymmetric(TreeNode* root) {
+        if (root == nullptr)
+            return true;
+
+        return isMirror(root->left, root->right);
+    }
+};
