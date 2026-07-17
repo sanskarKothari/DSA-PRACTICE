3a831fc3acb0207ee7a69c22692e625d828dabda
Sanskar Sachin Kothari <124ee0045@nitrkl.ac.in>
2026-07-17 12:07:31 +0530

    Time: 0 ms (100.00%) | Memory: 71.8 MB (52.67%) - LeetSync

---
+++ 53-maximum-subarray/maximum-subarray.cpp
@@ -0,0 +1,17 @@
+
+
+class Solution {
+public:
+    int maxSubArray(vector<int>& nums) {
+        int maxsum=INT_MIN; int currentsum=0;
+        for(int k:nums){
+            currentsum+=k;
+           maxsum=max(maxsum,currentsum);
+           if(currentsum<0){
+            currentsum=0;
+           }
+
+        }
+        return maxsum;
+    }
+};
