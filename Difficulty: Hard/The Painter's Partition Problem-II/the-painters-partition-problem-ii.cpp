1ad346b87500cf47860e5aa471975478252438ae
Sanskar Sachin Kothari <124ee0045@nitrkl.ac.in>
2026-07-18 13:15:52 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Hard/The Painter's Partition Problem-II/the-painters-partition-problem-ii.cpp
@@ -0,0 +1,44 @@
+class Solution {
+    bool ok(vector<int> &arr, long long m, int k) {
+
+    int students = 1;
+    int sum = 0;
+
+    for (int pages : arr) {
+
+        if (sum + pages <= m) {
+            sum += pages;
+        }
+        else {
+            students++;
+            sum = pages;
+        }
+    }
+
+    return students <= k;
+}
+  public:
+    int minTime(vector<int>& arr, int k) {
+        // code here
+        int n=arr.size();
+         if (k > n)
+            return -1;
+        
+        long long l= *max_element(arr.begin(), arr.end());
+        long long h=accumulate(arr.begin(), arr.end(), 0);
+        long long ans=-1;
+        while(l<=h){
+            long long m=l+(h-l)/2;
+            if(ok(arr,m,k)){
+                ans=m;
+                h=m-1;
+            }
+            else{
+                l=m+1;
+                
+            }
+        }
+        return ans;
+        
+    }
+};
