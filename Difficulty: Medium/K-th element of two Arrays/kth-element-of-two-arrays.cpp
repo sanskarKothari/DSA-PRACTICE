c60a965cc43c79868b4fe9038f3beb098eb0e646
Sanskar Sachin Kothari <124ee0045@nitrkl.ac.in>
2026-07-18 13:46:38 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Medium/K-th element of two Arrays/kth-element-of-two-arrays.cpp
@@ -0,0 +1,35 @@
+class Solution {
+  public:
+    int kthElement(vector<int> &A, vector<int> &B, int k) {
+        // code here
+        if (A.size() > B.size())
+    swap(A, B);
+
+int n = A.size();
+int m = B.size();
+
+int low = max(0, k - m);
+int high = min(k, n);
+
+while (low <= high) {
+
+    int cut1 = (low + high) / 2;
+    int cut2 = k - cut1;
+
+    int l1 = (cut1 == 0) ? INT_MIN : A[cut1 - 1];
+    int l2 = (cut2 == 0) ? INT_MIN : B[cut2 - 1];
+
+    int r1 = (cut1 == n) ? INT_MAX : A[cut1];
+    int r2 = (cut2 == m) ? INT_MAX : B[cut2];
+
+    if (l1 <= r2 && l2 <= r1)
+        return max(l1, l2);
+
+    else if (l1 > r2)
+        high = cut1 - 1;
+
+    else
+        low = cut1 + 1;
+}
+    }
+};
