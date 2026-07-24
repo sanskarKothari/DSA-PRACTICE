fa628da9c826e5542f07fd81c4cbde5108de221a
Sanskar Sachin Kothari <124ee0045@nitrkl.ac.in>
2026-07-24 15:02:45 +0530

    Time: 8 ms (54.67%) | Memory: 23.1 MB (17.05%) - LeetSync

--- 907-koko-eating-bananas/koko-eating-bananas.cpp
+++ 907-koko-eating-bananas/koko-eating-bananas.cpp
@@ -1,29 +1,28 @@
 class Solution {
-    bool ok(vector<int>& piles,int h,int m){
-        long long sum=0;
+    bool ok(int m,vector<int>& piles,int h){
+        long long sp=0;
         for(auto i:piles){
-            sum+=(i+m-1)/m;
-        }
-        return sum<=h;
+            sp+=(i+m-1)/m;
 
+        }
+        return sp<=h;
     }
 public:
-    int minEatingSpeed(vector<int>& piles, int hr) {
-        int l=1;
-        int h=*max_element(piles.begin(),piles.end());
+    int minEatingSpeed(vector<int>& piles, int h) {
+        int s=1;
+           int e=*max_element(piles.begin(),piles.end());
         int ans=-1;
-
-        while(l<=h){
-            int m=l+(h-l)/2;
-            if(ok(piles,hr,m)){
+        while(s<=e){
+            int m=s+(e-s)/2;
+            if(ok(m,piles,h)){
                 ans=m;
-                h=m-1;
-
+                e=m-1;
             }
             else{
-                l=m+1;
+                s=m+1;
             }
         }
-        return ans;
+       return ans;
+        
     }
 };
