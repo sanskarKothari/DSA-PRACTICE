6f80ef0ba129712d081723607b6e02de8fdb8aa1
Sanskar Sachin Kothari <124ee0045@nitrkl.ac.in>
2026-07-24 15:11:52 +0530

    Time: 20 ms (69.87%) | Memory: 70.5 MB (79.68%) - LeetSync

--- 1605-minimum-number-of-days-to-make-m-bouquets/minimum-number-of-days-to-make-m-bouquets.cpp
+++ 1605-minimum-number-of-days-to-make-m-bouquets/minimum-number-of-days-to-make-m-bouquets.cpp
@@ -1,58 +1,38 @@
 class Solution {
-public:
-
-    bool ok(vector<int>& a, int d, int m, int k) {
-
-        int c = 0;
-        int b = 0;
-
-        for (int x : a) {
-
-            if (x <= d) {
-
+    bool ok(int k,int mt,vector<int>& bloomDay,int m){
+        int c=0,b=0;
+        for(auto i:bloomDay){
+            if(i<=m){
                 c++;
-
-                if (c == k) {
+                if(c==k){
                     b++;
-                    c = 0;
+                    c=0;
+
                 }
-
-            } else {
-
-                c = 0;
+            }
+            else{
+                c=0;
             }
         }
-
-        return b >= m;
+        return b>=mt;
     }
-
-    int minDays(vector<int>& a, int m, int k) {
-
-        long long need = 1LL * m * k;
-
-        if (need > a.size())
-            return -1;
-
-        int s = *min_element(a.begin(), a.end());
-        int e = *max_element(a.begin(), a.end());
-
-        int ans = -1;
-
-        while (s <= e) {
-
-            int mid = s + (e - s) / 2;
-
-            if (ok(a, mid, m, k)) {
-
-                ans = mid;
-                e = mid - 1;    
-
-            } else {
-
-                s = mid + 1;
+public:
+    int minDays(vector<int>& bloomDay, int mt, int k) {
+        int l=*min_element(bloomDay.begin(),bloomDay.end());
+        int h=*max_element(bloomDay.begin(),bloomDay.end());
+        int ans=-1;
+        while(l<=h){
+            int m=l+(h-l)/2;
+            if(ok(k,mt,bloomDay,m)){
+                ans=m;
+                h=m-1;
+            }
+            else{
+                l=m+1;
             }
         }
-
         return ans;
+        
+        
     }
 };
