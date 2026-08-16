1a49caa2a49fee4a8d673de36ac09a312649b0be
Sanskar Sachin Kothari <124ee0045@nitrkl.ac.in>
2026-08-16 14:50:37 +0530

    Time: 43 ms (88.49%) | Memory: 24 MB (93.88%) - LeetSync

---
+++ 3442-maximum-total-reward-using-operations-i/maximum-total-reward-using-operations-i.cpp
@@ -0,0 +1,26 @@
+class Solution {
+public:
+    int maxTotalReward(vector<int>& rewardValues) {
+        sort(rewardValues.begin(), rewardValues.end());
+
+        int maxSum = 4000; // maximum possible useful sum
+        vector<bool> dp(maxSum + 1, false);
+
+        dp[0] = true;
+
+        for (int r : rewardValues) {
+            for (int x = r - 1; x >= 0; x--) {
+                if (dp[x]) {
+                    dp[x + r] = true;
+                }
+            }
+        }
+
+        for (int x = maxSum; x >= 0; x--) {
+            if (dp[x])
+                return x;
+        }
+
+        return 0;
+    }
+};
