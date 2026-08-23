e47d265a2231d36e757ec625d646e0e1c0f63233
Sanskar Sachin Kothari <124ee0045@nitrkl.ac.in>
2026-08-23 09:15:54 +0530

    Time: 0 ms (100.00%) | Memory: 9.6 MB (49.61%) - LeetSync

---
+++ 2395-longest-binary-subsequence-less-than-or-equal-to-k/longest-binary-subsequence-less-than-or-equal-to-k.cpp
@@ -0,0 +1,27 @@
+class Solution {
+public:
+    int longestSubsequence(string s, int k) {
+        int n = s.size();
+        int ans = 0;
+        long long val = 0;
+        long long power = 1;
+
+        // Take bits from right to left
+        for (int i = n - 1; i >= 0; i--) {
+            if (s[i] == '0') {
+                ans++;
+            }
+            else {
+                if (val + power <= k) {
+                    val += power;
+                    ans++;
+                }
+            }
+
+            if (power <= k)
+                power *= 2;
+        }
+
+        return ans;
+    }
+};
