8120fdbd3256e0b08ac1beb5978ec5e5242cfff8
Sanskar Sachin Kothari <124ee0045@nitrkl.ac.in>
2026-09-13 19:42:57 +0530

    Time: 75 ms (37.22%) | Memory: 16 MB (21.97%) - LeetSync

--- 864-image-overlap/image-overlap.cpp
+++ 864-image-overlap/image-overlap.cpp
@@ -1,23 +1,25 @@
 class Solution {
 public:
     int largestOverlap(vector<vector<int>>& A, vector<vector<int>>& B) {
-        int n = A.size(), ans = 0;
+        int n = A.size();
+        vector<pair<int,int>> a, b;
 
-        for (int dx = -n + 1; dx < n; dx++) {
-            for (int dy = -n + 1; dy < n; dy++) {
-                int cnt = 0;
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < n; j++) {
+                if (A[i][j]) a.push_back({i, j});
+                if (B[i][j]) b.push_back({i, j});
+            }
+        }
 
-                for (int i = 0; i < n; i++) {
-                    for (int j = 0; j < n; j++) {
-                        int x = i + dx, y = j + dy;
+        map<pair<int,int>, int> mp;
+        int ans = 0;
 
-                        if (x >= 0 && x < n && y >= 0 && y < n &&
-                            A[i][j] && B[x][y])
-                            cnt++;
-                    }
-                }
+        for (auto x : a) {
+            for (auto y : b) {
+                int dx = y.first - x.first;
+                int dy = y.second - x.second;
 
-                ans = max(ans, cnt);
+                ans = max(ans, ++mp[{dx, dy}]);
             }
         }
 
